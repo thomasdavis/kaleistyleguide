e2199cd78b785a8fdda8f7dee3f1b0a95b525b85
Thomas Davis <thomasalwyndavis@gmail.com>
2012-06-05 21:42:06 +1000

    example works on any server

--- js/config.js
+++ js/config.js
@@ -1,4 +1,7 @@
 define({
-	css_path: 'http://thomasdavis.github.com/kalei/css/'
+	// This css path is used as a default by the Kalei project it self.
+	css_path: window.location.origin + window.location.pathname + '/css'
+
+	// You can configure any path by just deleteing the one above and uncommenting the one below to point at your css directory
 	//css_path: 'http://localhost/kalei/css/'
 });
