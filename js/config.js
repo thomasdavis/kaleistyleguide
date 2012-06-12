b5d33d86671006570dee433b6a24f786f3817867
Thomas Davis <thomasalwyndavis@gmail.com>
2012-06-12 21:32:55 +1000

    ops broke kalei

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 define({
 	// This css path is used as a default by the Kalei project it self.
-//	css_path: window.location.origin + window.location.pathname + '/css',
+	css_path: window.location.origin + window.location.pathname + '/css',
 
 	// You can configure any path by just deleteing the one above and uncommenting the one below to point at your css directory
-	css_path: 'http://localhost/io/css/',
+	//css_path: 'http://localhost/io/css/',
 
 	disqus_shortname: 'kaleistyleguide'
 });
