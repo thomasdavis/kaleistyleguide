3bc87cf0161e25f9b7f67da431047f1e04b36752
Thomas Davis <thomasalwyndavis@gmail.com>
2012-11-07 14:31:04 +0000

    added link to nadarei

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 define({
 	// This css path is used as a default by the Kalei project it self.
-	css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) + window.location.pathname +  'css/styles.css'
+	//css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) + window.location.pathname +  'css/styles.css'
 	// You can configure any path by just deleteing the one above and uncommenting the one below to point at your css directory
-	//css_path: 'http://localhost/repos/budder-client/css/',
+	css_path: 'http://110.174.208.24/repos/apiengine-client/css/styles.css',
 
 	//disqus_shortname: 'kaleistyleguide'
 });
