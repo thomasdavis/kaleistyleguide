4fc714afc3efc058ac00c83e1bdfce1ae0faf756
Thomas Davis <thomasalwyndavis@gmail.com>
2013-08-21 16:36:28 +1000

    reverted back to including all the css

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 define({
 	// This css path is used as a default by the Kalei project it self.
-	//css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) + window.location.pathname +  'css/styles.css',
+	css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) + window.location.pathname +  'css/styles.css',
 	// You can configure any path by just deleteing the one above and uncommenting the one below to point at your css directory
-	css_path: 'http://localhost/repos/apiengine-ui/src/css/apiengine.css',
+	//css_path: 'http://localhost/css/styles.css',
   // You can manually list the css files to process by giving a css_paths array
 	//css_paths: ['../css/styles.css', '../css/buttons.css'],
 	
