4121f74a6d0348619694a1b262bb74dd4ca48c9b
pospi <pospi@spadgos.com>
2014-05-03 18:20:37 +1000

    swap path again

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ define({
 	css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) + window.location.pathname +  'css/styles.css',
 
 	// You can configure any path by just deleting the one above and uncommenting the one below to point at your css directory
-	// css_path: 'http://pospi.booty/kaleistyleguide/css/styles.css',
+	// css_path: 'http://localhost/kaleistyleguide/css/styles.css',
 
 	// You can manually list the css files to process by giving a css_paths array
 	//css_paths: ['../css/styles.css', '../css/buttons.css'],
