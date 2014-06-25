18b23c962edc5e7922621618d09f0a1f8aeaaa60
pospi <pospi@spadgos.com>
2014-06-25 14:54:59 +1000

    add a codepath to the menu view for pulling out LESS file menu groups

--- js/views/style/menu.js
+++ js/views/style/menu.js
@@ -1,13 +1,16 @@
+// :TODO: share parsed style data with page.js so we don't need to process it twice.
+//        There is a lot of duplicate code between the two at the moment
 define([
   'jquery',
   'underscore',
   'backbone',
   'text!templates/style/menu.html',
   'jscssp',
+'less',
   'config',
   'libs/marked/marked'
 ],
-function($, _, Backbone, dashboardPageTemplate, jscssp, config, marked) {
+function($, _, Backbone, dashboardPageTemplate, jscssp, less, config, marked) {
     var DashboardPage = Backbone.View.extend({
         el: '.kalei-style-menu',
         render: function () {
@@ -18,64 +21,131 @@ function($, _, Backbone, dashboardPageTemplate, jscssp, config, marked) {
               config.css_path = config.css_paths[0]
             }
 
-            require(['text!' + config.css_path], function (styles) {
-                var masterStyle = config.css_path.substr(config.css_path.lastIndexOf('/')+1);
-                var markedOpts = _.extend({ sanitize: false, gfm: true }, config.marked_options || {});
+            var configDir, styleUrl, masterStyle = config.css_path.substr(config.css_path.lastIndexOf('/')+1);
 
-                var parser = new jscssp();
-                marked.setOptions(markedOpts);
-                var stylesheet = parser.parse(styles, false, true);
-                var menus = [];
-                var menuTitle = '';
-                var currentMenu = {
-                    sheets: [],
-                    category: ''
-                };
+            if (masterStyle.substr(0,1) === '/') {
+                // non relative
+                configDir = config.css_path.substr(0, config.css_path.lastIndexOf('/'));
+                var pUrl = parseuri(configDir);
+                styleUrl = pUrl.protocol + '://' + pUrl.host + (pUrl.port === '' ? '' : ':'+ pUrl) + masterStyle;
+            } else {
+                configDir = config.css_path.substr(0, config.css_path.lastIndexOf('/'));
+                styleUrl = configDir + '/' + masterStyle;
+            }
 
-                _.each(stylesheet.cssRules, function(rule) {
-                    if(rule.type === 101) {
-                        var comment = rule.parsedCssText;
-                        comment = comment.replace('/*', '');
-                        comment = comment.replace('*/', '');
+            var markedOpts = _.extend({ sanitize: false, gfm: true }, config.marked_options || {});
+            marked.setOptions(markedOpts);
+            var menus = [];
+            var menuTitle = '';
+            var currentMenu = {
+                sheets: [],
+                category: ''
+            };
 
-                        var comments = marked.lexer(comment);
-                        var defLinks = comments.links || {};
-                        _.each(comments, function (comment) {
-                            var tokens = [comment];
-                            tokens.links = defLinks;
-
-                            if(comment.type === 'heading' && comment.depth === 1) {
-                                menuTitle = marked.parser(tokens);
-                            }
-                            if(comment.type === 'heading' && comment.depth === 3) {
-                                menus.push(_.extend({}, currentMenu));
-                                currentMenu.sheets = [];
-                                currentMenu.category = marked.parser(tokens);
-                            }
-
-                        });
-
-                    }
-                    if(rule.type === 3) {
-                        var sheet = rule.href.substr(rule.href.indexOf('(')+2, rule.href.indexOf(')')-rule.href.indexOf('(')-3);
-                        currentMenu.sheets.push(sheet);
-                    }
-                });
-
+            function finishMenu()
+            {
                 if(config.css_paths) {
                   for(var i = 1; i < config.css_paths.length; i++) {
                     currentMenu.sheets.push(config.css_paths[i])
                   }
                 }
                 menus.push(currentMenu);
-
+    console.log(menus);
                 $(that.el).html(_.template(dashboardPageTemplate, {_:_, menuTitle: menuTitle, menus: menus, entry: masterStyle}));
                 $('[href="' + window.location.hash + '"]').addClass('active');
                 if(window.location.hash === '') {
-                $('.js-kalei-home').addClass('active');
+                    $('.js-kalei-home').addClass('active');
                 }
-            });
+            }
 
+            switch (config.css_processor) {
+                case 'jscssp':
+
+                    require(['text!' + config.css_path], function (styles) {
+                        var parser = new jscssp();
+                        var stylesheet = parser.parse(styles, false, true);
+
+                        _.each(stylesheet.cssRules, function(rule) {
+                            if(rule.type === 101) {
+                                var comment = rule.parsedCssText;
+                                comment = comment.replace('/*', '');
+                                comment = comment.replace('*/', '');
+
+                                var comments = marked.lexer(comment);
+                                var defLinks = comments.links || {};
+                                _.each(comments, function (comment) {
+                                    var tokens = [comment];
+                                    tokens.links = defLinks;
+
+                                    if(comment.type === 'heading' && comment.depth === 1) {
+                                        menuTitle = marked.parser(tokens);
+                                    }
+                                    if(comment.type === 'heading' && comment.depth === 3) {
+                                        menus.push(_.extend({}, currentMenu));
+                                        currentMenu.sheets = [];
+                                        currentMenu.category = marked.parser(tokens);
+                                    }
+
+                                });
+
+                            }
+                            if(rule.type === 3) {
+                                var sheet = rule.href.substr(rule.href.indexOf('(')+2, rule.href.indexOf(')')-rule.href.indexOf('(')-3);
+console.log('found import', sheet);
+                                currentMenu.sheets.push(sheet);
+                            }
+                        });
+
+                        finishMenu();
+                    });
+
+                    break;
+
+                case 'less':
+
+                    require(['text!' + styleUrl], function (styles) {
+                        var parser = new(less.Parser)({
+                                filename: styleUrl,
+                                rootpath: configDir + '/',
+                                relativeUrls: true,
+                                insecure: true,
+                                paths: [configDir + '/'], // Specify search paths for @import directives
+                            });
+
+                        parser.parse(styles, function (err, tree) {
+                            _.each(tree.rules, function(rule) {
+                                if (rule.silent === false){ //Comment block
+                                    var comment = rule.value;
+                                    comment = comment.replace('/*', '');
+                                    comment = comment.replace('*/', '');
+
+                                    var comments = marked.lexer(comment);
+                                    var defLinks = comments.links || {};
+                                    _.each(comments, function (comment) {
+                                        var tokens = [comment];
+                                        tokens.links = defLinks;
+
+                                        if(comment.type === 'heading' && comment.depth === 1) {
+                                            menuTitle = marked.parser(tokens);
+                                        }
+                                        if(comment.type === 'heading' && comment.depth === 3) {
+                                            menus.push(_.extend({}, currentMenu));
+                                            currentMenu.sheets = [];
+                                            currentMenu.category = marked.parser(tokens);
+                                        }
+
+                                    });
+                                } else if (rule.path != null) { //Import Rule
+                                    currentMenu.sheets.push(rule.path.value);
+                                }
+                            });
+
+                            finishMenu();
+                        });
+                    });
+
+                    break;
+            }
         },
         events: {
             'click a.kalei-styleguide-menu-link': function (ev) {
